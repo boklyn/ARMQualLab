a853ff20322e61dc098346200633b9cd8e399516
json2 <beck.jason@outlook.com>
2016-12-06 10:13:54 -0800

    Update Troubleshoot1.1.ps1
    Updating template URIs

--- 200/Troubleshoot1.1.ps1
+++ 200/Troubleshoot1.1.ps1
@@ -1,5 +1,10 @@
-﻿$resourceGroupName = "TroubleshootingRG"
-$templateFileURI = "NEED TO UPDATE"
+$resourceGroupName = "TroubleshootingRG"
+
+# Uncomment to create the resource group if it does not already exist
+#$location = "westus"
+#New-AzureRmResourceGroup -ResourceGroupName $resourceGroupName -Location $location
+
+$templateFileURI = "https://raw.githubusercontent.com/boklyn/ARMQualLab/master/200/Template1.json"
 $parameters = @{}
 
 $parameters.Add("storageAccountType","Standrd_GRS")
