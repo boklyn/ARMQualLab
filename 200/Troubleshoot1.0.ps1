ca7d05240503adbea5e620a49a677091abeb0aa5
json2 <beck.jason@outlook.com>
2016-12-05 22:07:25 -0800

    Upload ARM Templates and Powershell Scripts
    All for troubleshooting

---
+++ 200/Troubleshoot1.0.ps1
@@ -0,0 +1,8 @@
+﻿$resourceGroupName = "TroubleshootingRG"
+$templateFileURI = "NEED TO UPDATE"
+$parameters = @{}
+
+$parameters.Add("storageAccountType","Standard_GRS")
+
+Test-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -Mode Incremental `
+                                    -TemplateUri $templateFileURI -TemplateParameterObject $parameters -Verbose
---
+++ 200/Troubleshoot3.0.ps1
@@ -0,0 +1,13 @@
+﻿$resourceGroupName = "TroubleshootingRG"
+$templateFileURI = "NEED TO UPDATE"
+$parameters = @{}
+
+$parameters.Add("storageAccountType","Standard_GRS")
+$parameters.Add("storageAccountName", "armquallabteststorageacct")
+
+$testResults = Test-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -Mode Incremental `
+                                   -TemplateUri $templateFileURI -TemplateParameterObject $parameters -Verbose
+
+Write-Host 
+Write-Host "Test Results" -ForegroundColor Green -NoNewline
+$testResults
