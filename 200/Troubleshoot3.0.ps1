2a92fde9517c51b2c744a23e3c74257af0ed11da
json2 <beck.jason@outlook.com>
2017-01-03 14:00:58 -0700

    Update Troubleshoot3.0.ps1
    Removing some code.

--- 200/Troubleshoot3.0.ps1
+++ 200/Troubleshoot3.0.ps1
@@ -9,10 +9,3 @@ $parameters = @{}
 
 $parameters.Add("storageAccountType","Standard_GRS")
 $parameters.Add("storageAccountName", "armquallabteststorageacct")
-
-$testResults = Test-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -Mode Incremental `
-                                   -TemplateUri $templateFileURI -TemplateParameterObject $parameters -Verbose
-
-Write-Host 
-Write-Host "Test Results" -ForegroundColor Green -NoNewline
-$testResults
