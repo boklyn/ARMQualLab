3a3e9677519acdb1b2435c4c8f9f26ad89690326
Anika Dhamodharan <anika.dhamo@gmail.com>
2017-09-24 17:42:08 -0700

    Add files via upload

---
+++ 200/Solutions/testingscript.ps1
@@ -0,0 +1,9 @@
+﻿$subid = "ENTER IN YOUR SUBSCRIPTION ID" 
+$location = "West US" 
+$newrgname = "testing" 
+ 
+Add-AzureRmAccount 
+Get-AzureRmSubscription -SubscriptionId $subid  | Set-AzureRmContext 
+New-AzureRmResourceGroup -Name $newrgname -Location $location
+
+New-AzureRmResourceGroupDeployment -ResourceGroupName $newrgname -Name "DeployName" -TemplateFile "ENTER IN LOCAL PATH TO JSON FILE"
